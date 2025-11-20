473dd6cfa4fff41de4259c46381b0d3240ed07bf
jhn322 <johan.soderlund96@gmail.com>
2025-11-21 00:28:32 +0100

    feat: Add Neon database serverless dependency and a new form component for data submission.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Hero from '../components/landing-page/Hero';
 import type { Metadata } from 'next';
+// import Form from './form';
 
 export const metadata: Metadata = {
   // * Metadata is inherited from layout.tsx
@@ -10,6 +11,8 @@ export default function Home() {
     <main id="main-content" className="flex min-h-screen flex-col">
       <header>
         <Hero />
+        {/* For testing Neon database connection */}
+      {/* <Form /> */}
       </header>
     </main>
   );
